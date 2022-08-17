ad3dd6bd53ba93a18f44d7e454fd62d88344798d
renaudatascience <85571576+renauddahou@users.noreply.github.com>
2022-08-17 20:52:17 +0200

    Update app1.py

--- app/app1.py
+++ app/app1.py
@@ -58,8 +58,9 @@ with st.sidebar.beta_expander("Analyse"):
         #subprocess.Popen("python collector.py"+" "+"-t"+" "+str(tag)+" "+"-c"+" "+str(country), shell=True)
         #subprocess.Popen("python collector.py"+" "+"-t"+" "+str(tag), shell=True)
         #time.sleep(10)
+        df="df"
         globals()[df]=scrapetweet(tag)
-        
+
 # Création d'une fonction pour compter le nombre de fois qu'un mot apparaît dans un tweet
 def count_word(word, text):
     word = word.lower()
